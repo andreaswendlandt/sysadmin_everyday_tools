923e843d4294728103e7197cd4b264d67d35a09a
andi <andreaswendlandt.79@googlemail.com>
2015-06-22 23:27:12 +0200

    mysql database backup

--- database_mysql_backup.sh
+++ database_mysql_backup.sh
@@ -6,8 +6,8 @@
 # ending for the dump
 NOW=$(date +"%Y%m%d")
 
-# with that you can check in the log 
-#how long the dump of a database took
+# with that you can check in the log
+# how long the dump of a database took
 function logtime() {
 echo "`date +%H:%M` o'clock"
 }
@@ -15,27 +15,32 @@ echo "`date +%H:%M` o'clock"
 # location of your dumps
 dump_folder="path_to_your_backup_directory"
 
-# as this script will dump all existing db's you can blacklist 
-#databases you don't want to dump
-database_blacklist=$(cat ${dump_folder}/blacklist | sed -e 's/ /|/g' 2>/dev/null)
+# for your own security his file should only be readable for root
+root_password=$(cat path_to_your_password 2>/dev/null)
+
+# as this script will dump all existing db's you can blacklist
+# databases you don't want to dump
+database_blacklist=$(cat $dump_folder/blacklist | sed -e 's/ /|/g' 2>/dev/null)
 
 if [ -z "$database_blacklist" ]; then
-  databases=$(sudo -u postgres psql -At -c "select datname from pg_database where not datistemplate and datallowconn order by datname;" 2>/dev/null)
+  databases=$(mysql -u root -p$root_password -e "show databases;" | egrep -v $databse_blacklist)
 else
-  databases=$(sudo -u postgres psql -At -c "select datname from pg_database where not datistemplate and datallowconn order by datname;" | egrep -v \ 
- "$database_blacklist" 2>/dev/null)
+  databases=$(mysql -u root -p$root_password -e "show databases;")
 fi
 
 # cleanup the dump folder
 find $dump_folder -mtime +14 -type f -exec rm {} \;
 
+
 for db in $databases
 do
   echo $db
   logtime
-  sudo -u postgres pg_dump -v --file=$dump_folder/${db}_${NOW} --format=custom --username=postgres --no-owner $db
+  mysqldump --single-transaction -u root  -p$root_password $db > $dump_folder/${db}_${NOW}.sql 
   echo "exit_code=$? $db"
   logtime
 done
+
+unset root_password
 
 exit 0
