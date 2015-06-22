adc30ca50e59c322a137c4a20d9c2635448e2880
andi <andreaswendlandt.79@googlemail.com>
2015-06-22 23:55:29 +0200

    nagios template

---
+++ nagios_template.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# author: guerillatux
+# desc: simple template to use for nagios checks
+# last modified: 24.5.2015
+
+# nagios knows 4 exit status
+OK_STATE=0
+CRITICAL_STATE=2
+WARNING_STATE=1
+UNKNOWN_STATE=3
+
+# first, check the amount of parameter in case you need them,
+# otherwise outcomment this section
+if [ $# -ne 3 ]; then
+  echo "error, this check needs 3 parameter"
+  echo "usage: $0 param1 param2 param3"
+  exit ${UNKNOWN_STATE}
+fi
+
+# perform the regular check
+if [ check something ]; then
+  echo "some comment"
+  exit ${WARNING_STATE}
+elif [ check something ]; then
+  echo "some comment"
+  exit ${OK_STATE}
+elif [ check something ]; then
+  echo "some comment"
+  exit ${CRITICAL_STATE}
+else
+  echo "could not determine the status"
+  exit ${UNKNOWN_STATE}
+fi
