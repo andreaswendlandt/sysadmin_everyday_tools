e12519c26f899b7dbd84f23aeef834dfda9f982e
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2017-03-16 11:35:03 +0100

    check the size of a file backed up in bacula

---
+++ bacula_check_file_size.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# author: andreas
+# desc: script for getting the size of a file backed up in bacula
+# last modified: 16.3.2017
+
+if [ $# -ne 1 ]; then
+    echo "ERROR, this script needs one parameter, the filename to check for"
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+file_to_check=$1
+
+filename_id=$(mysql -u root -e "select FilenameID from bacula.Filename where name='${file_to_check}';" | tail -n1)
+
+lstat=$(mysql -u root -e "select LStat from bacula.File where FilenameId='${filename_id}';" | head -n2| tail -n1 | awk '{print $8}')
+
+result=$(./lstat.php $lstat)
+
+echo "the size of $1 is $result bytes"
+exit 0
