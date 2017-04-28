2e88efd225b9cc49f416f1e3779c2b05c7867010
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2017-04-28 12:31:31 +0200

    create iwatch xml config and restart the iwatch daemon

--- create_iwatch_xml_config_file.sh
+++ create_iwatch_xml_config_file.sh
@@ -15,9 +15,12 @@ echo "<?xml version=\"1.0\" ?>
     <title>Transferdirectory</title>
     <contactpoint email="root@localhost" name=\"Administrator\"/>" >$configfile
 user=$(ls -1 /path_to_your_surveilled_directory)
-echo -n "$user" | while read line; do
+echo -e "$user" | while read line; do
     unix_user=$(ls -la /path_to_your_surveilled_directory | grep "$line" | awk '{print $3}')
     echo "    <path type=\"recursive\" alert=\"off\" syslog=\"on\" exec=\"/usr/local/sbin/transfer.sh '/path_to_your_surveilled_directory/"$line"/' /path_to_your_target_dir/"$unix_user"/\" events=\"create,move,close_write\">/path_to_your_surveilled_directory/"$line"</path>">>/$configfile
 done
 echo "</watchlist>
 </config>" >>/$configfile
+service iwatch stop
+sleep 5
+service iwatch start && echo "a new iwatch config was created and the daemon was restarted" || echo "something went wrong with restarting the daemon, please check manually"
