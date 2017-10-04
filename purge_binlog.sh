fb45ebdaa87caf5d965a4b5dfd779432818f37d8
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2017-10-04 14:04:57 +0200

    purge binlog

--- purge_binlog.sh
+++ purge_binlog.sh
@@ -7,7 +7,7 @@ used_space=$(df -h | grep mysql | awk '{print $5}' | sed 's/\%//')
 if [ $used_space -ge 90 ]; then
     mysql_root_pw=$(cat /root/.mysql_root_pw)
     back_in_time=$(date --date="2 hours ago" +%Y-%m-%d\ %T)
-    if mysql -uroot -p$mysql_root_pw -e "PURGE BINARY LOGS BEFORE \'$back_in_time\';" >/dev/null; then
+    if mysql -uroot -p$mysql_root_pw -e "PURGE BINARY LOGS BEFORE '$back_in_time';" >/dev/null; then
         echo $(date) "...gelöscht" >/tmp/purge.log
     else
         echo $(date) "konnte nichts löschen, bitte manuell prüfen" >/tmp/purge.log
