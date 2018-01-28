691ed1a014fb24415e0a3ada4217aa0b229480d8
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-01-28 22:28:12 +0100

    sms gateway

---
+++ sms_gateway/create_sms.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# author: guerillatux
+# desc: creates a file that can be copied to a server
+# desc: where a sms gateway is running (smstools)
+# last modified: 28.01.2018
+
+if [ $# -ne 2 ]; then
+    echo "ERROR, this script needs two parameters"
+    echo "Usage: $0 <88884444> <\"your text\">"
+    exit 1
+fi
+
+# check that the given phone number contains only digits
+not_a_valid_number=$(echo $1 | sed -e 's/[[:digit:]]//g')
+if [ $not_a_valid_number ]; then
+    echo "Your phone number contains other characters than digits, please try again"
+    exit 1
+fi  
+
+# create the sms file and copy it to your gateway server
+sms_file=sms_$(date +%s)
+echo -e "To: $1\n\n$2" >$sms_file
+scp $sms_file user@your_server:/path/to/dest
---
+++ sms_gateway/move-sms.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# author: guerillatux
+# desc: this script is triggered from iwatch and copies a file from a directory to the outgoing directory of smstools
+# last modified: 28.01.2018
+
+logfile="/var/log/iwatch/log"
+temp_file=/tmp/sms
+
+log(){
+  echo $(date) "$@" >>$logfile
+}
+
+for file in $(ls -1 "$1"); do
+    if grep "$file" $temp_file >/dev/null 2>&1; then
+        continue
+    fi
+    echo "$file" >>$temp_file
+    rsync -abz "${1}/${file}" "${2}/${file}" && rm -r "${1}/${file}" || log "$file could not be moved from $1 to $2"
+    sed -i "/$file/d" ${temp_file}
+done
