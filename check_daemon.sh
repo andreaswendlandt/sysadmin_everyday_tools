780925b56921b86be8a324dc1dc5fbcb292f3c62
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2019-01-19 21:56:12 +0100

    unified variablenaming, improved regex for the daemon variable, added comments

--- check_daemon.sh
+++ check_daemon.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 # author: guerillatux
 # desc: simple nagios check for ensuring that a given service/daemon is running
-# last modified: 13.07.2016
+# last modified: 19.01.2019
 
-OK_STATE=0
-CRITICAL_STATE=2
-WARNING_STATE=1
-UNKNOWN_STATE=3
+# possible exit codes
+ok_state=0
+critical_state=2
+warning_state=1
+unknown_state=3
 
+# ensure that a service/daemon is given
 if [ $# -ne 1 ]; then
     echo "WARNING, this check needs a service/daemon name to check for"
     echo "usage: $0 <service_name>"
-    exit ${WARNING_STATE}
+    exit ${warning_state}
 fi
 
-daemon=$(ls -1 /etc/init.d/ | grep $1)
+# what do we want to check for
+daemon=$(ls -1 /etc/init.d/ | grep "\b$1\b")
 
+# check the actual state of the given service/daemon in case it is present on the system
 if [ "$daemon" == "$1" ]; then
     if /etc/init.d/$1 status 2>/dev/null | egrep 'is running|active \(running\)' >/dev/null 2>&1; then
         echo "OK, service $1 is running"
-        exit ${OK_STATE}
+        exit ${ok_state}
     elif /etc/init.d/$1 status 2>/dev/null | egrep 'is not running|is stopped|inactive \(dead\)' >/dev/null 2>&1; then
         echo "CRITICAL, service $1 is not running!!!"
-        exit ${CRITICAL_STATE}
+        exit ${critical_state}
     else
       echo "UNKNOWN, could not determine the state of $1"
-      exit ${UNKNOWN_STATE}
+      exit ${unknown_state}
     fi
 else
     echo "UNKNOWN, the service $1 is not present on that system"
-    exit ${UNKNOWN_STATE}
+    exit ${unknown_state}
 fi
