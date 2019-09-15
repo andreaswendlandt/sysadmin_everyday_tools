3d5d50c383247672ef5082313d9de30a6d9c3520
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2019-09-15 22:11:02 +0200

    help function for grepping search patterns in config files

---
+++ config_grep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# author: guerillatux
+# desc: function for grepping in (config)files for a search pattern and check if the line where the search pattern
+# desc: occurs is outcommented
+# last modified: 15.9.2019
+
+search_pattern=$1
+search_file=$2
+
+config_grep(){
+    if [ $# -ne 2 ]; then
+        echo "error: 2 parameters are needed"
+        echo "USAGE: ${FUNCNAME} 'search pattern' 'search file'"
+        return 1
+    fi
+    if [ -f $2 ]; then
+        result=$(grep -i $1 $2 2>/dev/null | awk '{if (substr($1,1,1) ~ /#|;/) {print "outcommented"} else {print "match"}}')
+        if [ -z $result ]; then
+            result="no occurence"
+        fi
+        echo $result
+    else
+        echo "$2 does not exist"
+        return 1
+    fi
+}
+
+config_grep $search_pattern $search_file
